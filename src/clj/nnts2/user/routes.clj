513cebcbe6a1516d865aee7ff96fdccda2a04916
Murtaza Akbari <murtazasakbari@gmail.com>
2019-08-29 17:00:51 +0530

    Add user to an org

--- src/clj/nnts2/user/routes.clj
+++ src/clj/nnts2/user/routes.clj
@@ -1,10 +1,10 @@
 (ns nnts2.user.routes
   (:require [compojure.core :refer [defroutes GET]]
             [ring.util.response :as res]
-            [nnts2.user.handler :refer [create info]]))
+            [nnts2.user.handler :as handler]))
 
 (defroutes routes
-           (GET "/user" [] create)
+  (GET "/user" [] handler/create)
 
-           (GET "/user-info" []
-              #(res/response (get-in % [:session :user-info]))))
+  (GET "/user-info" []
+       #(res/response (get-in % [:session :user-info]))))
