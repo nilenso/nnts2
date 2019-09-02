3d241aeb958d293ae3b3819c10a78e33d6f90349
Sezal Jain <sezaljain90@gmail.com>
2019-09-02 12:21:45 +0530

    add more tests

--- test/clj/nnts2/note/functional_test.clj
+++ test/clj/nnts2/note/functional_test.clj
@@ -10,7 +10,7 @@
 
 
 (deftest create-note-success
-  (testing "String title, content and valid user id -> note success"
+  (testing "Should create note successfully for valid params"
     (let [request {:nnts-user 1 :body {:title "note-title" :content "note-content"}}
           {:keys [body status]} (create request)
           input (into (:body request) {:created-by-id 1})]
@@ -18,8 +18,24 @@
       (is (= input (select-keys body [:title :content :created-by-id]))))))
 
 (deftest create-note-failure
-  (testing "Given integer title, receive 400 error with string explanation "
+  (testing "Should receive 400 error with string explanation for an integer title"
     (let [request {:nnts-user 1 :body {:title 12344 :content "note-content"}}
           {:keys [body status]} (create request)]
       (is (= status 400))
-      (is (string? body)))))
+      (is (string? body))))
+  (testing "Should receive 400 error for missing title"
+    (let [request {:nnts-user 1 :body {:content "only-content"}}
+          {:keys [status body]} (create request)]
+      (is (= status 400))
+      (is (string? body))))
+  (testing "Should receive 400 error for missing content"
+    (let [request {:nnts-user 1 :body {:title "only-titlet"}}
+          {:keys [status body]} (create request)]
+      (is (= status 400))
+      (is (string? body))))
+  (testing "Should throw postgres exception  without nnts-user"
+    (let [request {:body {:title "no user" :content "no user"}}]
+      (is (thrown?  org.postgresql.util.PSQLException (create request)))))
+  (testing "Should throw postgres exception  without existing nnts-user"
+    (let [request {:nnts-user 2 :body {:title "non-existent user" :content "non-existent user"}}]
+      (is (thrown?  org.postgresql.util.PSQLException (create request))))))
