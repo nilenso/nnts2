3a59699cfb50b7db5ee2008d8432d4b9c30cbeb6
Sezal Jain <sezaljain90@gmail.com>
2019-10-14 11:04:22 +0530

    adding script to add default user directories to all notes

--- scripts/migrate_notes_without_directories.clj
+++ scripts/migrate_notes_without_directories.clj
@@ -1,11 +1,42 @@
 (ns migrate-notes-without-directories
-  (:require [nnts2.db.note :as note-db]))
+  (:require [nnts2.model.organization :as org-model]
+            [nnts2.model.directory :as dir-model]
+            [nnts2.model.note :as note-model]
+            [nnts2.db.note :as note-db]))
 
                                         ;All users will have a "Home" org. with a general channel.
                                         ;all notes without directories would be moved to the above general channel
 
 (defn get-notes-wo-directories []
-  (note-db/get {:directory nil}))
+  (note-db/get {:directory-id nil}))
 
 
-(get-notes-wo-directories)
+(defn create-general-directory-under-private-org-for-user [user-id]
+  (let [{org-id :id}         (org-model/create-org-add-membership
+                              {:name "MyNotes" :slug (str "MyNotes-" user-id)}
+                              user-id)
+        dir-data             {:org-id org-id :name "general" :parent-id nil :created-by-id user-id}
+        existing-general-dir (first (dir-model/list dir-data))]
+
+    {:directory-id (:id (if existing-general-dir
+                          existing-general-dir
+                          (dir-model/create dir-data)))}))
+
+
+(defn create-orgs-dirs-for-users [users]
+  (reduce
+   (fn [acc user-id]
+     (conj
+      acc
+      {user-id
+       (create-general-directory-under-private-org-for-user user-id)}))
+   {}
+   users))
+
+
+(defn run []
+  (let [notes                 (get-notes-wo-directories)
+        unique-users-of-notes (set (map :created-by-id notes))
+        users-with-dirs       (create-orgs-dirs-for-users unique-users-of-notes)]
+    (for [item notes]
+      (note-db/update (users-with-dirs (:created-by-id item)) {:id (:id item)}))))
